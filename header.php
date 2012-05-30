520dd43f3e4a9efdccc5c2f6689758cb5f0eb03c
mikefrancis <castblackjacket@gmail.com>
2012-05-30 14:49:35 +0200

    Added in Google HTML5 shiv

--- header.php
+++ header.php
@@ -7,5 +7,8 @@
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php wp_head(); ?>
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
   </head>
   <body <?php body_class(); ?>>
