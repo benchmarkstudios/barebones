99119a316eb1cd4e10131dc724d6b31d29fce173
mikefrancis <castblackjacket@gmail.com>
2012-05-30 12:33:00 +0200

    Sorting out tabbing...argh!

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
     <script src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
-	  <?php wp_footer(); ?>
-	</body>
+    <?php wp_footer(); ?>
+  </body>
 </html>
