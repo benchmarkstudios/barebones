45fb3d679fd0f8f191960871dbe4321be01bf4c0
Mike Francis <castblackjacket@gmail.com>
2012-11-13 23:58:14 +0000

    Fix path

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 	</footer>
 	<script src="//code.jquery.com/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/_/js/libs/jquery.min.js"><\/script>')</script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/js/script.js"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/_/js/script.js"></script>
 	<?php wp_footer(); ?>
 	</body>
 </html>
