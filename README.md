17ff78b20e2bb2ed071d2dec8f15d7ec5af05922
Mike Francis <castblackjacket@gmail.com>
2012-11-13 23:53:02 +0000

    Clean up

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # barebones
 
-An (almost) blank WordPress theme for HTML5 and beyond. Great as a starting point for any project with powerful features to encourage rapid development.
+A lightweight, skinny WordPress theme for HTML5 and beyond. Great as a starting point for any project with powerful features to encourage rapid development for most projects
 
 ## Features
 
 * Base reset and normalisation styles
 * Index and page Loop templates
 * Customised functions file containing useful starting points
+* Tidy and organised folder structure
+* Comes pre-bundled with latest CDN version of jQuery and fallback
--- header.php
+++ header.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
-        <title><?php wp_title( ' &ndash; ', true, 'right' ); bloginfo( 'name' ); ?></title>
-        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php wp_head(); ?>
-        <!--[if lt IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-    </head>
-    <body <?php body_class(); ?>>
-    	<header class="header">
-        	<div class="container">
-        		<h1><a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-                <nav>
-                	<ul class="nav">
-                    	<?php wp_list_pages( 'title_li=' ); ?>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+		<title><?php wp_title( ' &ndash; ', true, 'right' ); bloginfo( 'name' ); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<?php wp_head(); ?>
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body <?php body_class(); ?>>
+		<header class="header">
+			<div class="container">
+				<h1><a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+				<nav>
+					<ul class="nav">
+					<?php wp_list_pages( 'title_li=' ); ?>
+					</ul>
+				</nav>
+			</div>
+		</header>
