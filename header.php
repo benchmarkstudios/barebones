0b2f6414e23397158576667f130eba5ae1f1d985
Mike Francis <mikeffrancis@gmail.com>
2013-07-04 22:32:29 +0100

    Change to rems

--- header.php
+++ header.php
@@ -4,8 +4,9 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0" />
 		<meta name="HandheldFriendly" content="true" />
-		<title><?php wp_title( ' &ndash; ', true, 'right' ); bloginfo( 'name' ); ?></title>
+		<title><?php wp_title( ' / ', true, 'right' ); bloginfo( 'name' ); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<link rel="stylesheet" href="http://basehold.it/21" />
 		<?php wp_head(); ?>
 		<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -14,7 +15,6 @@
 	<body <?php body_class(); ?>>
 		<header class="header" role="banner">
 			<div class="container">
-				<a href="<?php echo site_url(); ?>"><img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>" /></a>
 				<nav role="navigation">
 					<ul class="nav">
 						<?php wp_list_pages( 'title_li=' ); ?>
