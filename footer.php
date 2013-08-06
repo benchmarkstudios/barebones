b13e4bd3113f36a82378857531b44cd6ac922bfb
Mike Francis <mikeffrancis@gmail.com>
2013-08-06 09:24:52 +0100

    Removing jQuery from footer.php

--- footer.php
+++ footer.php
@@ -3,9 +3,6 @@
 			<p>&copy; <?php bloginfo( 'name' ); ?> <?php echo date( 'Y' ); ?></p>
 		</div>
 	</footer>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_url' ); ?>/_/js/libs/jquery.min.js"><\/script>')</script>
-	<script src="<?php bloginfo( 'template_url' ); ?>/_/js/script.js"></script>
 	<?php wp_footer(); ?>
 	</body>
 </html>
