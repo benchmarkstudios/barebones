e0c8b891269cc00299fb32202c0618f5ce164332
Mike Francis <mikeffrancis@gmail.com>
2013-08-06 09:24:03 +0100

    jQuery (the right way)

--- functions.php
+++ functions.php
@@ -14,6 +14,14 @@ remove_action( 'wp_head', 'index_rel_link' );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' );
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
+function theme_scripts() {
+  wp_deregister_script( 'jquery' );
+  wp_register_script( 'jquery', ( 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js' ), false, '1.7.1', true);
+  wp_enqueue_script( 'jquery' );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
 function remove_comments_rss( $for_comments ) {
     return;
 }
