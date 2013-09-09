f4b77066e5822679aeaa84a18b1784fa997e5cbd
Mike Francis <mikeffrancis@gmail.com>
2013-09-09 16:12:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-$(function() {
+(function($) {
 
-	
+  $('li:last-child,td:last-child,tr:last-child,th:last-child').addClass('last');
+  $('td:nth-child(odd),th:nth-child(odd),tr:nth-child(odd)').addClass('odd');
+  $('td:nth-child(even),th:nth-child(even),tr:nth-child(even)').addClass('even');
 
-});
+  //
+
+})(jQuery);
