6ddc1d17ba5e7f189d52b61f4602cc38cc3dc5fe
Mike Francis <mikeffrancis@gmail.com>
2013-10-12 12:31:09 +0100

    Little JS comment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 (function($) {
 
+	// Add utility classes to lists/tables
+
   $('li:last-child,td:last-child,tr:last-child,th:last-child').addClass('last');
   $('td:nth-child(odd),th:nth-child(odd),tr:nth-child(odd)').addClass('odd');
   $('td:nth-child(even),th:nth-child(even),tr:nth-child(even)').addClass('even');
