73fca3b34d062e4336136f63f58b09c8ac1b06af
Mike Francis <mikeffrancis@gmail.com>
2013-11-21 09:06:43 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0" />
-		<meta name="HandheldFriendly" content="true" />
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
+		<meta name="HandheldFriendly" content="true">
+		<!--[if IE ]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+                <![endif]-->
 		<title><?php wp_title( ' / ', true, 'right' ); bloginfo( 'name' ); ?></title>
-		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 		<?php wp_head(); ?>
 		<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
