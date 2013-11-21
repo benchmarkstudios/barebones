8f5a2082572a6701d0007a76d7e41d1ff9227d33
Mike Francis <mikeffrancis@gmail.com>
2013-11-21 09:09:05 +0000

    Update header.php

--- header.php
+++ header.php
@@ -4,11 +4,13 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
 		<meta name="HandheldFriendly" content="true">
+		<meta name="MobileOptimized" content="320">
 		<title><?php wp_title( ' / ', true, 'right' ); bloginfo( 'name' ); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 		<?php wp_head(); ?>
 		<!--[if lt IE 9]>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 	</head>
