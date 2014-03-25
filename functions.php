12770ca6bd6745db5c75382a48e600a7168959e1
Mike Francis <mikeffrancis@gmail.com>
2014-03-25 09:49:29 +0000

    Removed extra parentheses

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ add_filter( 'post_comments_feed_link', 'remove_comments_rss' );
 
 function theme_scripts() {
   wp_deregister_script( 'jquery' );
-  wp_register_script( 'jquery', ( '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js' ), false, '1.10.2', true);
+  wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true);
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), null, true );
 }
