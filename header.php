e2116839f8e17856f59e052df189c2773f872f04
Mike Francis <mike@benchmark.co.uk>
2014-03-31 16:19:42 +0100

    converted all tabs to spaces

--- header.php
+++ header.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-		<meta name="HandheldFriendly" content="true">
-		<meta name="MobileOptimized" content="320">
-		<title><?php wp_title( '' ); ?></title>
-		<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-		<?php wp_head(); ?>
-		<!--[if lt IE 9]>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
-		<![endif]-->
-	</head>
-	<body <?php body_class(); ?>>
-		<header class="header" role="banner">
-			<div class="container">
-				<nav role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
-				</nav>
-			</div>
-		</header>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="320">
+    <title><?php wp_title( '' ); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+    <?php wp_head(); ?>
+    <!--[if lt IE 9]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+    <![endif]-->
+  </head>
+  <body <?php body_class(); ?>>
+    <header class="header" role="banner">
+      <div class="container">
+        <nav role="navigation">
+          <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+        </nav>
+      </div>
+    </header>
