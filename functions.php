de8ebf5b8cc356f5e236d7b72a33e5f1c4be513d
Mike Francis <mikeffrancis@gmail.com>
2014-04-08 13:29:05 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -29,8 +29,11 @@ add_filter( 'post_comments_feed_link', 'remove_comments_rss' );
 // jQuery the right way
 
 function theme_scripts() {
+  // If you need 'em
+  // wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Font+Family:Weight' );
+  // wp_enqueue_style( 'icons', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
   wp_deregister_script( 'jquery' );
-  wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true);
+  wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true );
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ), null, true );
 }
@@ -41,11 +44,11 @@ add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 if ( function_exists( 'register_nav_menus' ) )
   register_nav_menus(array(
-    'main' => 'Main Navigation'
-    // Add more menus here
+    'main'   => 'Main',
+    'footer' => 'Footer'
   ));
 
-function my_wp_nav_menu_args( $args = '' ) {
+function wp_nav_menu_args( $args = '' ) {
   $args['container']       = false;
   $args['container_class'] = false;
   $args['menu_id']         = false;
@@ -53,4 +56,4 @@ function my_wp_nav_menu_args( $args = '' ) {
   return $args;
 }
 
-add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
+add_filter( 'wp_nav_menu_args', 'wp_nav_menu_args' );
