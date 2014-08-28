742d310e9b19e9584bbac4f734a5d309c970396c
Mike Francis <mikeffrancis@gmail.com>
2014-08-28 12:10:05 +0100

    Added IE/Chrome edge

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <title><?php wp_title( '' ); ?></title>
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
