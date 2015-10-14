2c65b0a917a86f04828cbc5e386b340e0bdffc3a
Pedro Reis <pedro@benchmark.co.uk>
2015-10-14 14:59:47 +0100

    Fixed header

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en-EN">
+<html <?php language_attributes(); ?>>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-        <title>Title</title>
+        <title><?php wp_title( '' ); ?></title>
         <link rel="dns-prefetch" href="//google-analytics.com">
         <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
         <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png">
