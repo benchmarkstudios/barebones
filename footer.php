6f0f79caa4adddfabace0fe7211fb10b590ad100
Pedro Reis <pdcreis@gmail.com>
2015-11-25 12:34:49 +0000

    Added 'nav--footer' class to footer nav

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         <footer class="footer" role="contentinfo">
             <div class="container">
-                <?php wp_nav_menu(['theme_location' => 'footer']); ?>
+                <?php wp_nav_menu(['theme_location' => 'footer', 'menu_class' => 'nav nav--footer']); ?>
                 &copy; <?php bloginfo( 'name' ); ?> <?php echo date( 'Y' ); ?>
             </div>
         </footer>
