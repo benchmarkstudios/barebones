1cb1a596bee1424e5151337de732bcd640e7d159
Lukas Juhas <hello@lukasjuhas.com>
2016-05-17 12:11:44 +0100

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
+// config - desktop notifications
+process.env.DISABLE_NOTIFIER = false;
+
+var gulp = require('gulp');
+var gulpImagemin = require('gulp-imagemin');
 var elixir = require('laravel-elixir');
 
 // Assets path
 elixir.config.assetsPath = 'assets';
 
+// Imagemin
+var Task = elixir.Task;
+elixir.extend('imagemin', function(config) {
+  new Task('imagemin', function() {
+    return gulp.src(elixir.config.assetsPath + '/images/**/*')
+        .pipe(gulpImagemin(config))
+        .pipe(gulp.dest('./images'));
+  }).watch('./assets/images/**/*');
+});
+
 // Run elixir tasks
 elixir(function(mix) {
     mix.sass('barebones.scss', 'style.css')
-       .scripts(['script.js'], 'js/script.min.js');
+       .scripts(['script.js'], 'js/script.min.js')
+       .imagemin();
 });
