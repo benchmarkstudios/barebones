8c4c5efbfb574ab44b002ae9151bc28f0e06eb40
Pedro Reis <pdcreis@gmail.com>
2016-06-24 10:03:02 +0100

    Updated HTML 5 Shiv link

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <![endif]-->
         <!--[if lt IE 9]>
         <script src="//css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
         <![endif]-->
     </head>
