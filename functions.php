5ca11e181f05cc7ecbc26e10d156d7f41ffc3877
Pedro Reis <pdcreis@gmail.com>
2018-04-19 11:39:20 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -69,7 +69,7 @@ add_filter('post_comments_feed_link', 'barebones_post_comments_feed_link');
 
 function barebones_enqueue_scripts() {
     // wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Font+Family' );
-    // wp_enqueue_style( 'icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+    // wp_enqueue_style( 'icons', '//use.fontawesome.com/releases/v5.0.10/css/all.css' );
     wp_enqueue_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.min.js?' . filemtime(get_stylesheet_directory() . '/js/scripts.min.js'), [], null, true);
 }
 
