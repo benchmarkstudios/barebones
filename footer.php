5e5baf138b02e706716f6a5488419f3e4edcdcd4
Pedro Reis <pdcreis@gmail.com>
2018-05-29 13:24:39 +0100

    Updates

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         <footer class="footer" role="contentinfo">
             <div class="container">
-                <nav class="footer__navigation">
-                    <?php wp_nav_menu(['theme_location' => 'footer', 'menu_class' => 'nav nav--footer']); ?>
+                <nav class="nav nav--footer">
+                    <?php wp_nav_menu(['theme_location' => 'footer', 'menu_class' => 'nav__list']); ?>
                 </nav>
                 &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>
             </div>
