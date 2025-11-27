1743f95a25e11eb4a53c50cbf26caad022883035
Dan Coulthard <dan@benchmark.co.uk>
2025-11-27 15:45:30 +0000

    Added wp_head

--- header.php
+++ header.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html>
+<!DOCTYPE html>we
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
         <header class="header" role="banner">
